b29906bf28b5553d86e9ff917775bb9881594a5a
CrumpetsNTea <t.botell@gmail.com>
2021-10-25 17:10:05 -0400

    assert equal assignment updated with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(" 🎉 Assertion Passed: " + actual + "===" + expected);
+    console.log(`🎉 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log(" 🙈 Assertion Failed: " + actual + "!==" + expected);
+    console.log(`🙈 Assertion Failed: ${actual} !==  ${expected}`);
   }
 };
 
