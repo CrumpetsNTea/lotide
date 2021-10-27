08861df2dcf7d3b1b7171eed355f2c28528b8565
CrumpetsNTea <t.botell@gmail.com>
2021-10-27 17:42:47 -0400

    working and tested for various scenarios regarding length and key values matching

--- eqObjects.js
+++ eqObjects.js
@@ -10,12 +10,18 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
+  for (const val of Object.keys(object1))
+    if (object1[val] !== object2[val]) {
+      return false;
+    }
   return true;
 };
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
-
-
-assertEqual(eqObjects(ab, ba), true); // => true
-assertEqual(eqObjects(ab, abc), false); // => false
+const bac = { b: "5", a: "7", c: "9"};
+const cab = { b: "4", a: "7", c: "9"};
+assertEqual(eqObjects(ab, ba), true); // => PASS true === true (lengths are same and key values are same)
+assertEqual(eqObjects(ab, abc), false); // PASS false === false (because lengths are different)
+assertEqual(eqObjects(bac, abc), false); // => PASS false === false (lengths are the same so it will continue - keys are not same so will fail)
+assertEqual(eqObjects(bac, abc), true); // =>  FAIL false !== true (lengths are same but key values are not)
