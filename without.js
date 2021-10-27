242c20d52aadde711ec71099077e7f62b05a1935
CrumpetsNTea <t.botell@gmail.com>
2021-10-26 20:45:26 -0400

    add test to make sure original array is not altered by without

--- without.js
+++ without.js
@@ -42,3 +42,8 @@ assertArraysEqual((without([1, 2, 3], [1])), [2, 3]); // => PASS 2,3 === 2,3
 assertArraysEqual((without([1, 2, "3"], [1])), [2, 3]); // => FAIL 2,"3" !== 2,3
 assertArraysEqual((without([1, 2, "3"], ["3"])), [1, 2]); // => PASS 1,2 === 1,2
 assertArraysEqual((without([1, 2, 3], [2])), [2, 3]); // => FAIL 1,3 === 2,3
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]); // was not altered because hello,world !== hello,world,lighthouse
