2780a3dcaf2e3141bcd06f814c9e5f891f423e92
CrumpetsNTea <t.botell@gmail.com>
2021-10-27 18:34:02 -0400

    add util and inspect to get data to output correctly. Everything working after tests

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -27,10 +27,11 @@ const eqObjects = function(object1, object2) {
 
 
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) { //uses the eqArrays function to compare these two arrays
-    console.log(`🎉 Assertion Passed: ${actual} === ${expected}`); //console.logs happy message if they are identical
+    console.log(`🎉 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`); //console.logs happy message if they are identical
   } else { //if they are not exact
-    console.log(`🙈 Assertion Failed: ${actual} !== ${expected}`); //console.logs sad message
+    console.log(`🙈 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`); //console.logs sad message
   }
 };
 
