7163ebeb39db38c61dfa1a1d53818f61653063b1
CrumpetsNTea <t.botell@gmail.com>
2021-10-27 13:03:15 -0400

    add final operation for if the array is odd. all checks are working and passing

--- middle.js
+++ middle.js
@@ -24,14 +24,17 @@ const middle = function(array) {
   if (array.length < 3) { //if array has less than three items
     return stuckInTheMiddle; //return empty array
   } else if (array.length % 2 === 0) { //if array is even
-    theMiddle = Math.floor(array.length / 2); //theMiddle is now equal to the result of the math operations
+    theMiddle = Math.floor(array.length / 2); //theMiddle is now equal to the result of the math operations //Math.floor is used here to provide a round number as just dividing array.length in half returns 3.5, if using the tests at the bottom here
     stuckInTheMiddle.push(array[theMiddle - 1], array[theMiddle]); //pushes the result of above minus one, and the result of above, thus pushing the two middle numbers to the empty array of stuckInTheMiddle
+  } else if (array.length % 2 !== 0) {
+    theMiddle = Math.floor(array.length / 2); //makes theMiddle equal to the exact middle index
+    stuckInTheMiddle.push(array[theMiddle]); //pushes theMiddle to stuckInTheMiddle
   }
   return stuckInTheMiddle; //returns the array of middle numbers
 };
 console.log(middle([1, 2])); // => []
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
-
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
 // if array has less than 3 items then it returns empty
 //if array length is even then return two middle elements
 //if odd then return exact middle element
