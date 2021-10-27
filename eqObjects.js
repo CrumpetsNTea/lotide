c85c39e0a5bf152f251908735cd44d87eacc3de5
CrumpetsNTea <t.botell@gmail.com>
2021-10-27 17:57:12 -0400

    realized mixing curly braces after for loop but implemented them and everything is still working

--- eqObjects.js
+++ eqObjects.js
@@ -10,10 +10,11 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
-  for (const val of Object.keys(object1))
+  for (const val of Object.keys(object1)) {
     if (object1[val] !== object2[val]) {
       return false;
     }
+  }
   return true;
 };
 const ab = { a: "1", b: "2" };
@@ -24,4 +25,5 @@ const cab = { b: "4", a: "7", c: "9"};
 assertEqual(eqObjects(ab, ba), true); // => PASS true === true (lengths are same and key values are same)
 assertEqual(eqObjects(ab, abc), false); // PASS false === false (because lengths are different)
 assertEqual(eqObjects(bac, abc), false); // => PASS false === false (lengths are the same so it will continue - keys are not same so will fail)
-assertEqual(eqObjects(bac, abc), true); // =>  FAIL false !== true (lengths are same but key values are not)
+assertEqual(eqObjects(bac, cab), true); // =>  FAIL false !== true (lengths are same but key values are not)
+
