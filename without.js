89060df791597026ff12cdd144991a93b741549a
CrumpetsNTea <t.botell@gmail.com>
2021-10-26 20:43:49 -0400

    final - all working and tested

--- without.js
+++ without.js
@@ -18,11 +18,27 @@ let assertArraysEqual = function(array1, array2) { //takes two arrays
   }
 };
 
-let without = function(source, itemsToRemove) {
-  let editedArray = source; //the new array intiially equals the source array
-// we need to loop through the arrays
-};
+//Instructions:
 
 //This function should take in a source array and a itemsToRemove array.
 //It should return a new array with only those elements from source
 //that are not present in the itemsToRemove array.
+
+let without = function(source, itemsToRemove) {
+  let editedArray = source; //the new array intiially equals the source array
+  // we need to loop through the arrays
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) { //if [j] is equal to [i] - meaning it has been selected for removal
+        editedArray.splice([i], 1); //removes one element, located at the matching index item of source
+      }
+    }
+  }
+  return editedArray; //returns new array with removals
+};
+
+//Test code
+assertArraysEqual((without([1, 2, 3], [1])), [2, 3]); // => PASS 2,3 === 2,3
+assertArraysEqual((without([1, 2, "3"], [1])), [2, 3]); // => FAIL 2,"3" !== 2,3
+assertArraysEqual((without([1, 2, "3"], ["3"])), [1, 2]); // => PASS 1,2 === 1,2
+assertArraysEqual((without([1, 2, 3], [2])), [2, 3]); // => FAIL 1,3 === 2,3
