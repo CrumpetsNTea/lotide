6a13d491346240aef2cf420787dd3a45e997a599
CrumpetsNTea <t.botell@gmail.com>
2021-11-01 17:13:16 -0400

    finished writing tests for tail, all working and providing correct output

--- tail.js
+++ tail.js
@@ -1,6 +1,6 @@
 const tail = (actual) => {
   let newArr = actual.slice(1); //creates a new array with the first item from passed in array removed
-  console.log(newArr); //returns new array
+  return newArr; //returns new array
 };
 
 module.exports = tail; //make this function exportable
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,6 +1,18 @@
-const assertEqual = require('../assertEqual'); //import assert equal
+const chai = require('chai');
+const assert = chai.assert;
 const tail = require('../tail'); //import tail function
 //Test
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); //creates a new array with Yo Yo removed
-assertEqual(words.length, 3); //Ensures that origin array is not changed (still has 3 items)
+describe("tail function", () => {
+  it("should return an empty array when passed an array with less than three items", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("should return [1, 2] when passed array [1, 2, 3]", () => {
+    assert.deepEqual(tail([1,2,3]), [2, 3]);
+  });
+  it("should return [2, 3, 4, 5] when passed array [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  it("should return [Lighthouse, Labs] when passed array [Yo Yo, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
