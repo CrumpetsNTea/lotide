3c245b35f711901f2121f9c2c5ded4d8034407fe
CrumpetsNTea <t.botell@gmail.com>
2021-11-01 16:27:15 -0400

    add mocha and chai functionality, and rewrote most tests for head.test.js to use mocha and chai. Wrote two different kinds of tests based on what we were shown in lecture today too

--- test/head.test.js
+++ test/head.test.js
@@ -1,7 +1,30 @@
-const assertEqual = require('../assertEqual'); //import assert Equal
-const head = require('../head'); //import head function
+const head = require('../head'); //import head
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
 
-assertEqual(head([5,6,7]), 5); //Should pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //Should pass
-assertEqual(head([]), 5); //Should fail
-assertEqual(head([5]), 5); //Should pass
+describe("head function", () => {
+  it("should return 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("should return 5 for [5, 6, 7]", () => {
+    const result = head([5, 6, 7]);
+    expect(result).to.deep.equal(5); //messed around with a different way of writing a test
+  });
+  it("should not return 6 for [5, 6, 7]", () => {
+    const result = head([5, 6, 7]);
+    expect(result).to.not.deep.equal(6); //messed around with a different way of writing a test
+  });
+  it("should return Hello when passed array with Hello at [0]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+// assertEqual(head([]), 5); //Should fail
+// assertEqual(head([5]), 5); //Should pass
+
+// it("should return null if not valid number", () => {
+//   const input = "😃";
+//   const expectedOutput = null;
+//   const result = round(input);
+  
+//   expect(result).to.equal(expectedOutput);
